a36a7aec178e0648547fdb826ad59e360196771b
jmelton15 <johnmelton.projects@gmail.com>
2021-02-12 23:21:38 -0500

    updated mail server os environ settings

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',"SUPERSECRETKEY")
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = secret_keys["mail_server"][0]
-app.config['MAIL_PASSWORD'] = secret_keys["mail_server"][1]
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_DEFAULT_SENDER'] = secret_keys['mail_server'][0]
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_USERNAME','smtp.gmail.com')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT',587)
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS', True)
+app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL', False)
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 
 mail = Mail(app)
 
