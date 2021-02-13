1346746014e23c07a70243efb80ea7fccc348827
jmelton15 <johnmelton.projects@gmail.com>
2021-02-13 02:04:24 -0500

    changed testing to false for mail server

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS', True)
 app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL', False)
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_SUPPRESS_SEND'] = os.environ.get('MAIL_SUPPRESS_SEND')
+app.config['TESTING'] = False
 
 
 mail = Mail(app)
