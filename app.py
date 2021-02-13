a2d502d74db94e13cca961826119dd48a4227a15
jmelton15 <johnmelton.projects@gmail.com>
2021-02-12 22:31:58 -0500

    updated os environ settings in app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from functions import check_session_status,common_flashes,delete_feedback,delete
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///authenticate'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///authenticate')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',secret_keys['secret_key'])
